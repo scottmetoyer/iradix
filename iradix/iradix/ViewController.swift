55265198e41837be262e3eebf945ab42746b450e
Scott Metoyer <scott.metoyer@gmail.com>
2016-07-09 21:05:10 -0700

    Added extension for creating colors

--- iradix/iradix/ViewController.swift
+++ iradix/iradix/ViewController.swift
@@ -16,14 +16,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         pad1View.layer.borderWidth = 1
-        pad1View.layer.borderColor = UIColor.grayColor().CGColor;
+        pad1View.layer.borderColor = UIColor(netHex:0x979797).CGColor;
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(netHex:Int) {
+        self.init(red:(netHex >> 16) & 0xff, green:(netHex >> 8) & 0xff, blue:netHex & 0xff)
+    }
+}
